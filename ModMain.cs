528c583d6adea52f462a7a89bfc25050511bc524
Yukkuri C <799433638@qq.com>
2021-02-08 20:39:08 +0800

    支持鼠标拖拽操作造爪子

--- ModMain.cs
+++ ModMain.cs
@@ -12,5 +12,21 @@ namespace DSPMod
         {
             Harmony.CreateAndPatchAll(typeof(ModMain));
         }
+
+        [HarmonyPatch(typeof(PlayerAction_Build), "CreatePrebuilds")]
+        [HarmonyPrefix]
+        static void AutoClickWhenRelease(PlayerAction_Build __instance)
+        {
+            var self = __instance;
+            var cmd = self.controller.cmd;
+
+            if (!(cmd.mode == 3
+                && cmd.stage == 1)) return;
+            if (VFInput.rtsConfirm.onUp)
+            {
+                var axis = Traverse.Create(typeof(VFInput)).Field("axis_button").GetValue<VFInput.InputAxis>();
+                axis.down[0] = true;
+            }
+        }
     }
 }
