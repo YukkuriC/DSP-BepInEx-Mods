fc6223282be775e793d92c31406f4a383854cbed
Yukkuri C <799433638@qq.com>
2021-02-11 01:32:42 +0800

    星图折跃技术初版
    - TODO: 明确强制航行模式触发条件
    - TODO: 多次传送后玩家大小不定（通常偏大）
    - TODO: 传送后恒星模型未正确卸载

--- ModMain.cs
+++ ModMain.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using BepInEx;
 using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace DSPMod
 {
@@ -8,9 +11,84 @@ namespace DSPMod
     [BepInProcess("DSPGAME.exe")]
     public partial class ModMain : BaseUnityPlugin
     {
+        static string[] ControlInfo = {
+            "按住左Ctrl单击传送至该天体",
+            "按住左Ctrl+Alt单击可在不聚焦视野的同时连续传送",
+        };
+
         void Start()
         {
             Harmony.CreateAndPatchAll(typeof(ModMain));
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(UIStarmap), "OnPlanetClick")]
+        static bool PlanetWarp(UIStarmapPlanet planet)
+        {
+            if (!Input.GetKey(KeyCode.LeftControl)) return true;
+
+            var dest = planet.planet;
+            Helper.PlayerWarp(dest.uPosition, dest.realRadius);
+
+            return !Input.GetKey(KeyCode.LeftAlt);
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(UIStarmap), "OnStarClick")]
+        static bool StarWarp(UIStarmapStar star)
+        {
+            if (!Input.GetKey(KeyCode.LeftControl)) return true;
+
+            var dest = star.star;
+            Helper.PlayerWarp(dest.uPosition, dest.physicsRadius * 1.5f);
+
+            return !Input.GetKey(KeyCode.LeftAlt);
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(UIStarmap), "UpdateCursorView")]
+        static void WarpTip(UIStarmap __instance)
+        {
+            var self = __instance;
+            var go = Traverse.Create(self).Field<RectTransform>("cursorViewTrans").Value;
+            var text = Traverse.Create(self).Field<Text>("cursorViewText").Value;
+
+            if (!go.gameObject.activeSelf) return;
+
+            // 判断是否已添加说明
+            if (text.text.Length > 0 && text.text[text.text.Length - 1] == '\r') return;
+
+            // 添加说明
+            text.text += '\n' + string.Join("\n", ControlInfo);
+            text.text += '\r';
+        }
+    }
+
+    public static class Helper
+    {
+        public static VectorLF3 RandSphere(float radius)
+        {
+            var randPos = UnityEngine.Random.onUnitSphere * radius;
+            return new VectorLF3(randPos.x, randPos.y, randPos.z);
+        }
+
+        public static void PlayerWarp(VectorLF3 center, float radius)
+        {
+            var target = center + Helper.RandSphere(radius);
+            var plr = GameMain.data.mainPlayer;
+            UIRoot.instance                                   // 随手薅的一个MonoBehavior_(:з」∠)_
+                .StartCoroutine(WarpFromGround(plr, target)); // 挂载传送协程
+        }
+
+        // 协程传送
+        static IEnumerator WarpFromGround(Player plr, VectorLF3 target)
+        {
+            if (!plr.sailing) // 起飞+短暂停顿
+            {
+                plr.movementState = EMovementState.Sail;
+                plr.warpState = 1; // 鸽子扭脖子螺旋桨.gif
+                plr.uPosition = (target + plr.uPosition) / 2;
+                plr.uVelocity = (target - plr.uPosition).normalized * 2000;
+                yield return new WaitForSeconds(0.2f);
+            }
+            plr.uPosition = target; // 写入坐标
         }
     }
 }
--- _Config.cs
+++ _Config.cs
@@ -2,8 +2,8 @@
 {
     public partial class ModMain
     {
-        const string UID = "yukkuric.dsp.mod";
-        const string NAME = "The Mod";
+        const string UID = "yukkuric.dsp.fasttravel";
+        const string NAME = "Fast Travel";
         const string VERSION = "1.0.0.0";
     }
 }
