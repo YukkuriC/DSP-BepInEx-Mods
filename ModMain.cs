da9ca6d849611e496e4831f77c4f1ffd904a278a
Yukkuri C <799433638@qq.com>
2021-02-10 14:42:34 +0800

    解锁巨星采集站建造位置限制

--- ModMain.cs
+++ ModMain.cs
@@ -12,5 +12,21 @@ namespace DSPMod
         {
             Harmony.CreateAndPatchAll(typeof(ModMain));
         }
+
+        [HarmonyPatch(typeof(PlayerAction_Build), "CheckBuildConditions")]
+        [HarmonyPostfix]
+        static void UnlockEquator(PlayerAction_Build __instance, ref bool __result)
+        {
+            bool newResult = true;
+            foreach (var preview in __instance.buildPreviews)
+            {
+                if (preview.condition != EBuildCondition.Ok)
+                {
+                    if (preview.condition == EBuildCondition.BuildInEquator) preview.condition = EBuildCondition.Ok;
+                    else newResult = false;
+                }
+            }
+            __result = newResult;
+        }
     }
 }
--- _Config.cs
+++ _Config.cs
@@ -2,8 +2,8 @@
 {
     public partial class ModMain
     {
-        const string UID = "yukkuric.dsp.mod";
-        const string NAME = "The Mod";
+        const string UID = "yukkuric.dsp.collector_everywhere";
+        const string NAME = "Collector Everywhere";
         const string VERSION = "1.0.0.0";
     }
 }
