5f1dc08b20212e619a9fd10877a0fc677138c53c
Yukkuri C <799433638@qq.com>
2021-02-11 10:17:44 +0800

    1.0.1.0
    - 改名
    - 稳定的强制航行方法
    - 拒绝如蜜传如蜜（行星）
    - 完善文字说明部分
    - TODO: 恒星卸载问题

--- ModMain.cs
+++ ModMain.cs
@@ -12,6 +12,7 @@ namespace DSPMod
     public partial class ModMain : BaseUnityPlugin
     {
         static string[] ControlInfo = {
+            "=============================================",
             "按住左Ctrl单击传送至该天体",
             "按住左Ctrl+Alt单击可在不聚焦视野的同时连续传送",
         };
@@ -27,6 +28,7 @@ namespace DSPMod
             if (!Input.GetKey(KeyCode.LeftControl)) return true;
 
             var dest = planet.planet;
+            if (dest == GameMain.localPlanet) return true;
             Helper.PlayerWarp(dest.uPosition, dest.realRadius);
 
             return !Input.GetKey(KeyCode.LeftAlt);
@@ -56,8 +58,11 @@ namespace DSPMod
             if (text.text.Length > 0 && text.text[text.text.Length - 1] == '\r') return;
 
             // 添加说明
-            text.text += '\n' + string.Join("\n", ControlInfo);
+            if (self.mouseHoverPlanet == null || self.mouseHoverPlanet.planet != GameMain.localPlanet)
+                text.text += '\n' + string.Join("\n", ControlInfo);
             text.text += '\r';
+            // 更新外框大小
+            go.sizeDelta = new Vector2(text.preferredWidth * 0.5f + 44f, text.preferredHeight * 0.5f + 14f);
         }
     }
 
@@ -80,15 +85,20 @@ namespace DSPMod
         // 协程传送
         static IEnumerator WarpFromGround(Player plr, VectorLF3 target)
         {
-            if (!plr.sailing) // 起飞+短暂停顿
+            // 从地面起飞
+            if (!plr.sailing)
             {
                 plr.movementState = EMovementState.Sail;
                 plr.warpState = 1; // 鸽子扭脖子螺旋桨.gif
-                plr.uPosition = (target + plr.uPosition) / 2;
-                plr.uVelocity = (target - plr.uPosition).normalized * 2000;
-                yield return new WaitForSeconds(0.2f);
+                var dir = (target - plr.uPosition).normalized;
+                plr.uPosition = GameMain.localPlanet.uPosition + dir * (GameMain.localPlanet.realRadius * 1.5f);
+                yield return new WaitForSeconds(0.1f);
             }
             plr.uPosition = target; // 写入坐标
+
+            // 大小校正
+            yield return new WaitForSeconds(0.1f);
+            plr.transform.localScale = Vector3.one;
         }
     }
 }
--- _Config.cs
+++ _Config.cs
@@ -3,7 +3,7 @@
     public partial class ModMain
     {
         const string UID = "yukkuric.dsp.fasttravel";
-        const string NAME = "Fast Travel";
-        const string VERSION = "1.0.0.0";
+        const string NAME = "Starmap Warp";
+        const string VERSION = "1.0.1.0";
     }
 }
