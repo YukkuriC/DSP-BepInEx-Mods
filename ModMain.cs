65b533e58a12f4ee956b829f670b76cc8d7256c6
Yukkuri C <799433638@qq.com>
2021-02-19 18:55:38 +0800

    自动填翘曲

--- ModMain.cs
+++ ModMain.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using BepInEx;
 using HarmonyLib;
 
@@ -11,6 +12,57 @@ namespace DSPMod
         void Start()
         {
             Harmony.CreateAndPatchAll(typeof(ModMain));
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(PlanetTransport), "GameTick")]
+        static void RefillWarper(PlanetTransport __instance)
+        {
+            var self = __instance;
+
+            // valid stations
+            var allStations = new List<StationComponent>();
+            for (int i = 1; i < self.stationCursor; i++)
+            {
+                var station = self.stationPool[i];
+                if (station == null || station.id != i) continue;
+                allStations.Add(station);
+            }
+
+            // supply
+            var supplyStations = new List<StationStore[]>();
+            var supplySlotIds = new List<int>();
+            int totalSupply = 0;
+            foreach (var station in allStations)
+            {
+                for (int sid = 0; sid < station.storage.Length; sid++)
+                {
+                    if (station.storage[sid].itemId != StationComponent.WARPER_ITEMID || station.storage[sid].count <= 0) continue;
+                    supplyStations.Add(station.storage);
+                    supplySlotIds.Add(sid);
+                    totalSupply += station.storage[sid].count;
+                }
+            }
+
+            // need
+            var needStations = new List<StationComponent>();
+            foreach (var station in allStations)
+            {
+                if (station.warperCount < station.warperMaxCount) needStations.Add(station);
+            }
+
+            // auto supply
+            int warpDrain = needStations.Count;
+            if (totalSupply >= warpDrain)
+            {
+                foreach (var station in needStations) station.warperCount++;
+                for (int p = 0; p < supplySlotIds.Count; p++)
+                {
+                    int dWarp = Math.Min(supplyStations[p][supplySlotIds[p]].count, warpDrain);
+                    supplyStations[p][supplySlotIds[p]].count -= dWarp;
+                    warpDrain -= dWarp;
+                    if (warpDrain <= 0) break;
+                }
+            }
         }
     }
 }
--- _Config.cs
+++ _Config.cs
@@ -2,8 +2,8 @@
 {
     public partial class ModMain
     {
-        const string UID = "yukkuric.dsp.mod";
-        const string NAME = "The Mod";
+        const string UID = "yukkuric.dsp.refillwarp";
+        const string NAME = "Refill Warp";
         const string VERSION = "1.0.0.0";
     }
 }
