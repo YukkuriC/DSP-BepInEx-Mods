5d15ca539e4d5d96e0dbcbcdadd550866ef4c001
Yukkuri C <799433638@qq.com>
2021-02-25 00:00:13 +0800

    全部潮汐锁定

--- ModMain.cs
+++ ModMain.cs
@@ -8,9 +8,28 @@ namespace DSPMod
     [BepInProcess("DSPGAME.exe")]
     public partial class ModMain : BaseUnityPlugin
     {
+        static EPlanetSingularity[] noTidal = {
+            EPlanetSingularity.TidalLocked2,
+            EPlanetSingularity.TidalLocked4,
+            EPlanetSingularity.ClockwiseRotate,
+        };
+
         void Start()
         {
             Harmony.CreateAndPatchAll(typeof(ModMain));
+        }
+
+        [HarmonyPostfix, HarmonyPatch(typeof(PlanetGen), "CreatePlanet")]
+        static void Lock(PlanetData __result, int orbitAround)
+        {
+            if (orbitAround != 0) return;
+            __result.rotationPeriod = __result.orbitalPeriod;
+            __result.singularity |= EPlanetSingularity.TidalLocked;
+            foreach (var flag in noTidal)
+            {
+                if ((__result.singularity & flag) != EPlanetSingularity.None)
+                    __result.singularity ^= flag;
+            }
         }
     }
 }
--- _Config.cs
+++ _Config.cs
@@ -2,8 +2,8 @@
 {
     public partial class ModMain
     {
-        const string UID = "yukkuric.dsp.mod";
-        const string NAME = "The Mod";
+        const string UID = "yukkuric.dsp.alltidal";
+        const string NAME = "All Tidal";
         const string VERSION = "1.0.0.0";
     }
 }
