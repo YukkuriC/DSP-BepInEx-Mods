2ed9cedf27c3f00cff780d93b09b254bec75123c
YukkuriC <799433638@qq.com>
2023-12-16 14:08:45 +0800

    禁用异常检查&自存档验证

--- ModMain.cs
+++ ModMain.cs
@@ -1,8 +1,9 @@
 ﻿using UnityEngine;
 using BepInEx;
 using HarmonyLib;
+using ABN;
 
-namespace DSPMod
+namespace NoCheck
 {
     [BepInPlugin(UID, NAME, VERSION)]
     [BepInProcess("DSPGAME.exe")]
@@ -11,6 +12,30 @@ namespace DSPMod
         void Start()
         {
             Harmony.CreateAndPatchAll(typeof(ModMain));
+        }
+
+        [HarmonyPrefix, HarmonyPatch(typeof(GameAbnormalityData_0925), nameof(GameAbnormalityData_0925.TriggerAbnormality))]
+        static bool NoTrigger()
+        {
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GameAbnormalityData_0925), nameof(GameAbnormalityData_0925.NothingAbnormal))]
+        [HarmonyPatch(typeof(AchievementLogic), "get_isSelfFormalGame")]
+        [HarmonyPatch(typeof(AchievementLogic), "get_active")]
+        static bool AlwaysTrue(ref bool __result)
+        {
+            __result = true;
+            return false;
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(GameAbnormalityData_0925), nameof(GameAbnormalityData_0925.IsAbnormalTriggerred))]
+        static bool AlwaysFalse(ref bool __result)
+        {
+            __result = false;
+            return false;
         }
     }
 }
--- _Config.cs
+++ _Config.cs
@@ -1,9 +1,9 @@
-﻿namespace DSPMod
+﻿namespace NoCheck
 {
     public partial class ModMain
     {
-        const string UID = "yukkuric.dsp.mod";
-        const string NAME = "The Mod";
+        const string UID = "yukkuric.dsp.no_check";
+        const string NAME = "No Abnormality Check";
         const string VERSION = "1.0.0.0";
     }
 }
